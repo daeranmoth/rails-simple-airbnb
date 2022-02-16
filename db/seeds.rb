168bd8131bca9060384482f44267354f147140c2
gary camboulives <gary.camboulives@gmail.com>
2022-02-16 17:04:24 +0100

    edcfvbghn

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Flat.create!(
+  name: 'Garden Flat London',
+  address: '12 Clifton Gardens London W9 1DT',
+  description: 'open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 95,
+  number_of_guests: 5
+)
